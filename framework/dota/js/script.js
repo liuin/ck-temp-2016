fc562f22eae171aa40f27fa812d15ddf0b1a13b2
liuin <20893791@qq.com>
2016-11-11 18:33:35 +0800

    1111

--- framework/dota/js/script.js
+++ framework/dota/js/script.js
@@ -93,23 +93,22 @@ Map.checkClick = function  (pos) {
   for (var i = 0;  i<Map.viewObjs.length; i++) {
     var obj = Map.viewObjs[i];
    
-    if ( posX > (obj.position.x - obj.size.width/2) && posX < (obj.position.x + obj.size.width/2) && posY > (obj.position.y - obj.size.height) && posY < (obj.position.y)) {            
-      if (clickObj != null) {        
+    if ( posX > (obj.position.x - obj.size.width/2) && posX < (obj.position.x + obj.size.width/2) && posY > (obj.position.y - obj.size.height) && posY < (obj.position.y)) {
+      if (clickObj != null) {
         if (clickObj.zIndex < Map.viewObjs[i].zIndex) {
           clickObj = Map.viewObjs[i];
           clickObjIndex = i;
         }
       }else {
-        clickObj = Map.viewObjs[i];  
+        clickObj = Map.viewObjs[i];
         clickObjIndex = i;
       }
-
       //return true
     }
   }
   
   if (clickObj != null) {
-    clickObj.clickEvent(pos, clickObjIndex); 
+    clickObj.clickEvent(pos, clickObjIndex);
     return true
   }
 
@@ -121,7 +120,7 @@ Map.build();
 
 //构建英雄
 function Hero () {
-  this.lift = 1000;
+  this.lift = 100;
   this.type = "physics";
   this.skill = {
     "1" : {
@@ -165,11 +164,103 @@ function Hero () {
     y: 10
   }
   //this.plan();
-
   //将此加入场景
   Map.viewObjs.push(this);
+}
+
+Hero.prototype.checkAction = function  () {
+  var $this = this;
+
+  
+  if ($this.status == 'move') {
+    return
+  }
+  
+  if($this.direction == "left"){            
+    $this.thisCkeckPosX = $this.position.x - $this.size.width/2 - $this.attackDistance;    
+    $this.thisCkeckPosXWidth = $this.attackDistance;
+    $this.thisCkeckPosY = $this.position.y - $this.size.height;    
+    $this.thisCkeckPosYHeight = $this.size.height;
+    
+    for (var i = 0;  i<Map.viewObjs.length; i++) {
+      var obj = Map.viewObjs[i];
+      
+      if($this === obj){
+        continue;
+      }      
+
+      var thisObjX = (obj.position.x - obj.size.width/2);
+      var thisObjXWidth = obj.size.width;
+      var thisObjY = (obj.position.y - obj.size.height);
+      var thisObjYHeight = obj.size.height;
+
+   
+      
+      //判断矩形碰撞
+      var ifImpoac = $this.checkRectImpoac($this.thisCkeckPosX,$this.thisCkeckPosY,$this.thisCkeckPosXWidth,$this.thisCkeckPosYHeight,thisObjX,thisObjY,thisObjXWidth,thisObjYHeight);
+
+      //console.log(ifImpoac);
+
+      //console.log($this.attackProTime);
+
+      if(ifImpoac && ($this.attackProTime == undefined)){
+        $this.attack(obj);
+      }      
+    }    
+  }
+  
+  if($this.direction == "right"){   
+  
+    $this.thisCkeckPosX = $this.position.x + $this.size.width/2 + $this.attackDistance/2;    
+    $this.thisCkeckPosXWidth = $this.attackDistance;
+    $this.thisCkeckPosY = $this.position.y - $this.size.height;    
+    $this.thisCkeckPosYHeight = $this.size.height;
+    
+    for (var i = 0;  i<Map.viewObjs.length; i++) {
+      var obj = Map.viewObjs[i];
+      
+      if($this === obj){
+        continue;
+      }      
+
+      var thisObjX = (obj.position.x - obj.size.width/2);
+      var thisObjXWidth = obj.size.width;
+      var thisObjY = (obj.position.y - obj.size.height);
+      var thisObjYHeight = obj.size.height;
+
+   
+      
+      //判断矩形碰撞
+      var ifImpoac = $this.checkRectImpoac($this.thisCkeckPosX,$this.thisCkeckPosY,$this.thisCkeckPosXWidth,$this.thisCkeckPosYHeight,thisObjX,thisObjY,thisObjXWidth,thisObjYHeight);
+
+      //console.log(ifImpoac);
+
+      //console.log($this.attackProTime);
+
+      if(ifImpoac && ($this.attackProTime == undefined)){
+        $this.attack(obj);
+      }      
+    }   
+  }
 
 }
+
+//判断矩形碰撞
+Hero.prototype.checkRectImpoac = function(x1,y1,w1,h1,x2,y2,w2,h2){
+  if ((x1 + w1) < x2) {  
+      return false;  
+  } else if ((x2 + w2) < x1) {  
+      return false;  
+  } else if ((y1 + h1) < y2) {  
+      
+      return false;  
+  } else if ((y2 + h2) < y1) {  
+      
+      return false;  
+  }
+  return true;  
+}
+
 
 Hero.prototype.clickEvent = function  (pos,number) {
   Map.objResetSelect();
@@ -178,14 +269,7 @@ Hero.prototype.clickEvent = function  (pos,number) {
   if (Map.viewObjsCurrent == this) {
     this.moveTo(pos.x, pos.y);
   }else{
-    Map.viewObjsCurrent = this;
-    
-//    for (var i = 0;  i<Map.viewObjs.length ; i++) {
-//      Map.viewObjs[i].zIndex = 1;
-//    }
-//
-//    Map.viewObjsCurrent.zIndex = 99;
-
+    Map.viewObjsCurrent = this;    
   }
 }
 
@@ -246,12 +330,14 @@ Hero.prototype.planWaiter = function  () {  //目前和planMove一样
 
 Hero.prototype.moveTo = function  (x,y) {
   this.status = "move";
+  this.attackProTime = undefined;
   var distanceX = x - this.position.x;
   var distanceY = y - this.position.y;
   var distance = Math.floor(Math.sqrt(distanceX*distanceX + distanceY*distanceY));
   
   if (distanceX == 0 && distanceY == 0) {
     this.status = "waiter";
+    this.attackProTime = undefined;
     return
   }
 
@@ -261,8 +347,14 @@ Hero.prototype.moveTo = function  (x,y) {
   this.moveX = x;
   this.moveY = y;
 
-  (this.moveX > this.position.x) ? (this.moveXDir = 1) : (this.moveXDir = -1);
-  (this.moveY > this.position.y) ? (this.moveYDir = 1) : (this.moveYDir = -1);  
+  if(this.moveX > this.position.x){
+    this.moveXDir = 1;
+    this.direction = "right";
+  }else {
+    this.moveXDir = -1;
+    this.direction = "left" ;
+  }
+  (this.moveY > this.position.y) ? (this.moveYDir = 1) : (this.moveYDir = -1);
 }
 
 Hero.prototype.moveToPro = function  (fps) {
@@ -307,6 +399,7 @@ Hero.prototype.moveToPro = function  (fps) {
 
 Hero.prototype.update = function  () {
   var $this = this;
+  $this.checkAction();
   switch ($this.status) {
   case 'move':
     $this.moveToPro(Engine.fps);
@@ -318,17 +411,17 @@ Hero.prototype.update = function  () {
 
   case 'waiter':
     this.planMove();
-  break
+  break  
 
   default:
+
   }
 }
 
 
 Hero.prototype.attack = function  (enemy) {
-
   if (typeof enemy == "undefined") {
-    this.status = "waiter";  
+    this.status = "waiter";      
     return
   }
 
@@ -352,41 +445,57 @@ Hero.prototype.attack = function  (enemy) {
   this.enemy = enemy;
   this.enemyMeasure = measure;
   this.attackInOut = false;
-
-  //this.attackStart = new Date().getTime();
-
-  //enemy.life -= measure;
-
-
-  //if (enemy.life < 0) {
-  //  this.dead();
-  //}
 }
 
 Hero.prototype.attackPro = function  (fps) {
   var $this = this;
   var speed = $this.attackSpeed / fps;
-
-  ($this.attackProTime === undefined) ? $this.attackProTime = speed : $this.attackProTime += speed;
   
   //前摆
   this.planMove();
 
+  var ca = Map.ctx;
+
+  ($this.attackProTime === undefined) ? $this.attackProTime = speed : $this.attackProTime += speed;
+
+  //画攻击
+  ca.save();
+  ca.translate($this.position.x,$this.position.y);
+  //ca.beginPath();
+
+
+  ca.fillStyle='rgba(255, 0, 0, '+ $this.attackProTime/$this.attackSpeed +')';
+
+  if ($this.direction == "left") {
+    //ca.arc(-$this.size.width/1.15,-$this.size.height/1.15,10,0,Math.PI*2,true);
+    ca.fillRect(-$this.size.width/1.15 - 5, -$this.size.height/1.15, 30, $this.size.height/1.15);
+  }
+
+  if ($this.direction == "right") {
+    //ca.arc($this.size.width/1.15,-$this.size.height/1.15,10,0,Math.PI*2,true);
+    ca.fillRect($this.size.width/1.15 - 25, -$this.size.height/1.15, 30, $this.size.height/1.15);
+  }
   
-
-
+  ca.restore();
 
   if ($this.attackProTime > $this.attackInTime && ($this.attackInOut == false)) {  
-    console.log($this.attackProTime,$this.attackInTime,$this.attackSpeed);
     $this.enemy.lift -= $this.enemyMeasure;
     $this.attackInOut = true;
+    if ($this.enemy.lift <= 0) {
+      $this.enemy.status = 'dead';
+      $this.enemy.position.x = -9999;
+      $this.enemy.position.y = -9999;
+      delete $this.enemy;
+    }
   }
 
   //后摆
-  this.planMove();
+  //this.planMove();
 
   if ($this.attackProTime > $this.attackSpeed) {
+    $this.attackProTime = undefined;
     $this.status = "waiter";
+    
   }
 
 
@@ -395,6 +504,16 @@ Hero.prototype.attackPro = function  (fps) {
 
 Hero.prototype.dead = function  () {
   console.log('dead');
+  var $this = this;
+  for (var i = 0;  i<Map.viewObjs.length ; i++) {
+    //console.log(Map.viewObjs[i].zIndex);
+    var obj = Map.viewObjs[i];
+    if ($this == obj) {
+      Map.viewObjs.splice(i);
+    }        
+  }
+  delete $this;
+
 }
 
 
@@ -408,13 +527,13 @@ function Attack() {
 var hero1 = new Hero();
 var hero2 = new Hero();
 
-hero1.position.x = -200;
+hero1.position.x = 150;
 hero1.position.y = 200;
 hero1.bgColor = "#f64";
 
 hero1.moveTo(200,200);
 
-hero2.position.x = Map.obj.width + 200;
+hero2.position.x = Map.obj.width - 100;
 hero2.position.y = 200;
 hero2.bgColor = "green";
 

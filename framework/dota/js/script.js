fd520bdfedbd5d001ed9dea306e30cffdfebc843
liuin <20893791@qq.com>
2016-11-22 14:12:57 +0800

    1122

--- framework/dota/js/script.js
+++ framework/dota/js/script.js
@@ -7,106 +7,105 @@ Map.ctx = Map.obj.getContext('2d');
 Map.viewObjsCurrent = null;
 Map.viewObjs = [];
 
-
-Map.build = function () {
+Map.build = function() {
   var ctx = Map.ctx;
   Map.obj.width = Map.width;
   Map.obj.height = Map.height;
   ctx.fillStyle = "#eee";
 }
 
-Map.objResetSelect = function  () {
-  for (var i = 0;  i<Map.viewObjs.length ; i++) {
+Map.objResetSelect = function() {
+  for (var i = 0; i < Map.viewObjs.length; i++) {
     Map.viewObjs[i].selected = false;
   }
 }
 
-Map.reset = function  () {
+Map.reset = function() {
   var ctx = Map.ctx;
   ctx.clearRect(0, 0, Map.width, Map.height);
   ctx.fillStyle = "#eee";
 }
 
-Map.windowToCanvas = function (x, y) {
+Map.windowToCanvas = function(x, y) {
   var ca = Map.obj;
   var bbox = ca.getBoundingClientRect();
   return {
-    x : (x - bbox.left) * (ca.width/bbox.width),
-    y : (y - bbox.top) * (ca.height/bbox.height)
+    x: (x - bbox.left) * (ca.width / bbox.width),
+    y: (y - bbox.top) * (ca.height / bbox.height)
   };
 }
 
-Map.planObjs = function  () {
+Map.planObjs = function() {
 
   var getQueue = [];
 
-  
 
-  for (var i = 0;  i<Map.viewObjs.length ; i++) {
-    
-    for (var n = (i + 1);  n<Map.viewObjs.length ; n++) {
-      var s = Map.viewObjs[n-1].zIndex;
-      
+
+  for (var i = 0; i < Map.viewObjs.length; i++) {
+
+    for (var n = (i + 1); n < Map.viewObjs.length; n++) {
+      var s = Map.viewObjs[n - 1].zIndex;
+
       var comp = Map.viewObjs[n].zIndex;
-      
+
       if (s > comp) {
-        
-        var temp = Map.viewObjs.splice((n-1),1)[0];
+
+        var temp = Map.viewObjs.splice((n - 1), 1)[0];
         Map.viewObjs.push(temp);
-      }    
+      }
     }
   }
 
-  for (var i = 0;  i<Map.viewObjs.length ; i++) {
+  for (var i = 0; i < Map.viewObjs.length; i++) {
     //console.log(Map.viewObjs[i].zIndex);
     Map.viewObjs[i].update();
-    
-    
+
+
   }
   //console.log(Map.viewObjs);
   //Engine.stop = true;
 
 
-  
+
 }
 
-Map.obj.onclick = function  (e) {
+Map.obj.onclick = function(e) {
   var $this = Map;
-  var pos = $this.windowToCanvas(e.clientX,e.clientY);
-  var check =  Map.checkClick(pos);
-  
+  var pos = $this.windowToCanvas(e.clientX, e.clientY);
+  var check = Map.checkClick(pos);
+
   if (check == false && Map.viewObjsCurrent) {
-    Map.viewObjsCurrent.moveTo(pos.x,pos.y);
+    Map.viewObjsCurrent.moveTo(pos.x, pos.y);
   }
 
   //hero1.moveTo(pos.x,pos.y);
   //hero1.selected = true;
 }
 
-Map.checkClick = function  (pos) {
+Map.checkClick = function(pos) {
   var posX = pos.x;
   var posY = pos.y;
 
   var clickObj = null;
   var clickObjIndex = null;
 
-  for (var i = 0;  i<Map.viewObjs.length; i++) {
+  for (var i = 0; i < Map.viewObjs.length; i++) {
     var obj = Map.viewObjs[i];
-   
-    if ( posX > (obj.position.x - obj.size.width/2) && posX < (obj.position.x + obj.size.width/2) && posY > (obj.position.y - obj.size.height) && posY < (obj.position.y)) {
+
+    if (posX > (obj.position.x - obj.size.width / 2) && posX < (obj.position.x + obj.size.width / 2) && posY > (obj.position.y - obj.size.height) && posY < (obj.position.y)) {
       if (clickObj != null) {
         if (clickObj.zIndex < Map.viewObjs[i].zIndex) {
           clickObj = Map.viewObjs[i];
           clickObjIndex = i;
         }
-      }else {
+      } else {
         clickObj = Map.viewObjs[i];
         clickObjIndex = i;
       }
       //return true
     }
   }
-  
+
   if (clickObj != null) {
     clickObj.clickEvent(pos, clickObjIndex);
     return true
@@ -119,28 +118,28 @@ Map.checkClick = function  (pos) {
 Map.build();
 
 //构建英雄
-function Hero () {
+function Hero() {
   this.lift = 100;
   this.type = "physics";
   this.skill = {
-    "1" : {
-      "img": "",
-      "introduct": "call file",
-      "power": 100
+      "1": {
+        "img": "",
+        "introduct": "call file",
+        "power": 100
+      },
+      "2": {
+        "img": "",
+        "introduct": "hello kity",
+        "power": 100
+      }
     },
-    "2" : {
-      "img":"",
-      "introduct" : "hello kity",
-      "power": 100
-    }
-  },
-  this.size = {
-    width : 100,
-    height : 130
-  },
+    this.size = {
+      width: 100,
+      height: 130
+    },
 
-  this.moveSpeed = 150,
-  this.bgColor = "f60";
+    this.moveSpeed = 150;
+    this.bgColor = "f60";
   this.moveSpeedX = this.moveSpeed || 1.5;
   this.moveSpeedY = this.moveSpeed || 1.5;
   this.moveX = null;
@@ -148,158 +147,158 @@ function Hero () {
   this.moveXDir = 1;
   this.moveYDir = 1;
   this.zIndex = 1;
-  this.attackSpeed = 1.5,
-  this.attackInTime = 0.7,
-  this.attackDistance = 50,
-  this.direction = "left",
-  this.normalPhysicsAttack = 20,
-  this.normalMagicAttack = 20,
-  this.physicsDefense = 0,
-  this.magicDefense = 10,
+  this.attackSpeed = 1.5;
+  this.attackInTime = 0.7;
+  this.attackDistance = 50;
+  this.direction = "left";
+  this.normalPhysicsAttack = 20;
+  this.normalMagicAttack = 20;
+  this.physicsDefense = 0;
+  this.magicDefense = 10;
   this.power = 0;
   this.selected = false;
   this.status = "waiter";
   this.position = {
-    x: 10,
-    y: 10
-  }
-  //this.plan();
-  //将此加入场景
+      x: 10,
+      y: 10
+    }
+    //this.plan();
+    //将此加入场景
   Map.viewObjs.push(this);
 }
 
-Hero.prototype.checkAction = function  () {
+Hero.prototype.checkAction = function() {
   var $this = this;
 
-  
+
   if ($this.status == 'move') {
     return
   }
-  
-  if($this.direction == "left"){            
-    $this.thisCkeckPosX = $this.position.x - $this.size.width/2 - $this.attackDistance;    
-    $this.thisCkeckPosXWidth = $this.attackDistance;
-    $this.thisCkeckPosY = $this.position.y - $this.size.height;    
-    $this.thisCkeckPosYHeight = $this.size.height;
-    
-    for (var i = 0;  i<Map.viewObjs.length; i++) {
-      var obj = Map.viewObjs[i];
-      
-      if($this === obj){
-        continue;
-      }      
 
-      var thisObjX = (obj.position.x - obj.size.width/2);
+  if ($this.direction == "left") {
+    $this.thisCkeckPosX = $this.position.x - $this.size.width / 2 - $this.attackDistance;
+    $this.thisCkeckPosXWidth = $this.attackDistance;
+    $this.thisCkeckPosY = $this.position.y - $this.size.height;
+    $this.thisCkeckPosYHeight = $this.size.height;
+
+    for (var i = 0; i < Map.viewObjs.length; i++) {
+      var obj = Map.viewObjs[i];
+
+      if ($this === obj) {
+        continue;
+      }
+
+      var thisObjX = (obj.position.x - obj.size.width / 2);
       var thisObjXWidth = obj.size.width;
       var thisObjY = (obj.position.y - obj.size.height);
       var thisObjYHeight = obj.size.height;
 
-   
-      
+
+
       //判断矩形碰撞
-      var ifImpoac = $this.checkRectImpoac($this.thisCkeckPosX,$this.thisCkeckPosY,$this.thisCkeckPosXWidth,$this.thisCkeckPosYHeight,thisObjX,thisObjY,thisObjXWidth,thisObjYHeight);
+      var ifImpoac = $this.checkRectImpoac($this.thisCkeckPosX, $this.thisCkeckPosY, $this.thisCkeckPosXWidth, $this.thisCkeckPosYHeight, thisObjX, thisObjY, thisObjXWidth, thisObjYHeight);
 
       //console.log(ifImpoac);
 
       //console.log($this.attackProTime);
 
-      if(ifImpoac && ($this.attackProTime == undefined)){
+      if (ifImpoac && ($this.attackProTime == undefined)) {
         $this.attack(obj);
-      }      
-    }    
+      }
+    }
   }
-  
-  if($this.direction == "right"){   
-  
-    $this.thisCkeckPosX = $this.position.x + $this.size.width/2 + $this.attackDistance/2;    
-    $this.thisCkeckPosXWidth = $this.attackDistance;
-    $this.thisCkeckPosY = $this.position.y - $this.size.height;    
-    $this.thisCkeckPosYHeight = $this.size.height;
-    
-    for (var i = 0;  i<Map.viewObjs.length; i++) {
-      var obj = Map.viewObjs[i];
-      
-      if($this === obj){
-        continue;
-      }      
 
-      var thisObjX = (obj.position.x - obj.size.width/2);
+  if ($this.direction == "right") {
+
+    $this.thisCkeckPosX = $this.position.x + $this.size.width / 2 + $this.attackDistance / 2;
+    $this.thisCkeckPosXWidth = $this.attackDistance;
+    $this.thisCkeckPosY = $this.position.y - $this.size.height;
+    $this.thisCkeckPosYHeight = $this.size.height;
+
+    for (var i = 0; i < Map.viewObjs.length; i++) {
+      var obj = Map.viewObjs[i];
+
+      if ($this === obj) {
+        continue;
+      }
+
+      var thisObjX = (obj.position.x - obj.size.width / 2);
       var thisObjXWidth = obj.size.width;
       var thisObjY = (obj.position.y - obj.size.height);
       var thisObjYHeight = obj.size.height;
 
-   
-      
+
+
       //判断矩形碰撞
-      var ifImpoac = $this.checkRectImpoac($this.thisCkeckPosX,$this.thisCkeckPosY,$this.thisCkeckPosXWidth,$this.thisCkeckPosYHeight,thisObjX,thisObjY,thisObjXWidth,thisObjYHeight);
+      var ifImpoac = $this.checkRectImpoac($this.thisCkeckPosX, $this.thisCkeckPosY, $this.thisCkeckPosXWidth, $this.thisCkeckPosYHeight, thisObjX, thisObjY, thisObjXWidth, thisObjYHeight);
 
       //console.log(ifImpoac);
 
       //console.log($this.attackProTime);
 
-      if(ifImpoac && ($this.attackProTime == undefined)){
+      if (ifImpoac && ($this.attackProTime == undefined)) {
         $this.attack(obj);
-      }      
-    }   
+      }
+    }
   }
 
 }
 
 //判断矩形碰撞
-Hero.prototype.checkRectImpoac = function(x1,y1,w1,h1,x2,y2,w2,h2){
-  if ((x1 + w1) < x2) {  
-      return false;  
-  } else if ((x2 + w2) < x1) {  
-      return false;  
-  } else if ((y1 + h1) < y2) {  
-      
-      return false;  
-  } else if ((y2 + h2) < y1) {  
-      
-      return false;  
+Hero.prototype.checkRectImpoac = function(x1, y1, w1, h1, x2, y2, w2, h2) {
+  if ((x1 + w1) < x2) {
+    return false;
+  } else if ((x2 + w2) < x1) {
+    return false;
+  } else if ((y1 + h1) < y2) {
+
+    return false;
+  } else if ((y2 + h2) < y1) {
+
+    return false;
   }
-  return true;  
+  return true;
 }
 
 
-Hero.prototype.clickEvent = function  (pos,number) {
+Hero.prototype.clickEvent = function(pos, number) {
   Map.objResetSelect();
-  this.selected = true; 
+  this.selected = true;
 
   if (Map.viewObjsCurrent == this) {
     this.moveTo(pos.x, pos.y);
-  }else{
-    Map.viewObjsCurrent = this;    
+  } else {
+    Map.viewObjsCurrent = this;
   }
 }
 
-Hero.prototype.planMove = function  () {
+Hero.prototype.planMove = function() {
   var $this = this;
   var ca = Map.ctx;
-  ca.fillStyle= this.bgColor;
+  ca.fillStyle = this.bgColor;
 
   ca.save();
-  ca.translate($this.position.x,$this.position.y);
+  ca.translate($this.position.x, $this.position.y);
 
   if (this.moveXDir == -1) {
-    ca.scale(-1, 1);    
+    ca.scale(-1, 1);
   }
 
- 
+
   if (this.selected == true) {
     ca.strokeStyle = "blue";
-    ca.strokeRect(-($this.size.width/2), (-$this.size.height), ($this.size.width), ($this.size.height));
+    ca.strokeRect(-($this.size.width / 2), (-$this.size.height), ($this.size.width), ($this.size.height));
   }
 
-  ca.fillRect((-$this.size.width/2), (-$this.size.height), ($this.size.width), ($this.size.height));
+  ca.fillRect((-$this.size.width / 2), (-$this.size.height), ($this.size.width), ($this.size.height));
 
   ca.fillStyle = "#fff";
-  ca.fillRect(($this.size.width/4), (-$this.size.height/1.1), ($this.size.width/5), ($this.size.height/5));
+  ca.fillRect(($this.size.width / 4), (-$this.size.height / 1.1), ($this.size.width / 5), ($this.size.height / 5));
 
   ca.beginPath();
-  ca.arc(0,0,10,0,Math.PI*2,true);
+  ca.arc(0, 0, 10, 0, Math.PI * 2, true);
   ca.closePath();
-  ca.fillStyle="#000";
+  ca.fillStyle = "#000";
   ca.fill();
 
   ca.restore();
@@ -308,57 +307,57 @@ Hero.prototype.planMove = function  () {
 
 
 
-Hero.prototype.planWaiter = function  () {  //目前和planMove一样
+Hero.prototype.planWaiter = function() { //目前和planMove一样
   var $this = this;
   var ca = Map.ctx;
-  ca.fillStyle= this.bgColor;
- 
+  ca.fillStyle = this.bgColor;
+
   if (this.selected == true) {
     ca.strokeStyle = "blue";
-    ca.strokeRect(($this.position.x - $this.size.width/2), ($this.position.y - $this.size.height), ($this.size.width), ($this.size.height));
+    ca.strokeRect(($this.position.x - $this.size.width / 2), ($this.position.y - $this.size.height), ($this.size.width), ($this.size.height));
   }
 
-  ca.fillRect(($this.position.x - $this.size.width/2), ($this.position.y - $this.size.height), ($this.size.width), ($this.size.height));
+  ca.fillRect(($this.position.x - $this.size.width / 2), ($this.position.y - $this.size.height), ($this.size.width), ($this.size.height));
 
 
   ca.beginPath();
-  ca.arc($this.position.x,$this.position.y,10,0,Math.PI*2,true);
+  ca.arc($this.position.x, $this.position.y, 10, 0, Math.PI * 2, true);
   ca.closePath();
-  ca.fillStyle="#000";
+  ca.fillStyle = "#000";
   ca.fill();
 }
 
-Hero.prototype.moveTo = function  (x,y) {
+Hero.prototype.moveTo = function(x, y) {
   this.status = "move";
   this.attackProTime = undefined;
   var distanceX = x - this.position.x;
   var distanceY = y - this.position.y;
-  var distance = Math.floor(Math.sqrt(distanceX*distanceX + distanceY*distanceY));
-  
+  var distance = Math.floor(Math.sqrt(distanceX * distanceX + distanceY * distanceY));
+
   if (distanceX == 0 && distanceY == 0) {
     this.status = "waiter";
     this.attackProTime = undefined;
     return
   }
 
-  this.moveSpeedX = distanceX/distance * this.moveSpeed;
-  this.moveSpeedY = distanceY/distance * this.moveSpeed;
+  this.moveSpeedX = distanceX / distance * this.moveSpeed;
+  this.moveSpeedY = distanceY / distance * this.moveSpeed;
 
   this.moveX = x;
   this.moveY = y;
 
-  if(this.moveX > this.position.x){
+  if (this.moveX > this.position.x) {
     this.moveXDir = 1;
     this.direction = "right";
-  }else {
+  } else {
     this.moveXDir = -1;
-    this.direction = "left" ;
+    this.direction = "left";
   }
   (this.moveY > this.position.y) ? (this.moveYDir = 1) : (this.moveYDir = -1);
 }
 
-Hero.prototype.moveToPro = function  (fps) {
-  
+Hero.prototype.moveToPro = function(fps) {
+
   var distanceX = this.position.x + this.moveSpeedX / fps - this.moveX;
   var distanceY = this.position.y + this.moveSpeedY / fps - this.moveY;
 
@@ -381,47 +380,47 @@ Hero.prototype.moveToPro = function  (fps) {
   }
 
 
-  if (checkEnd) {    
+  if (checkEnd) {
     this.position.x = this.moveX;
     this.position.y = this.moveY;
     //console.log(this.position);
     this.status = "waiter";
-  }else {
+  } else {
     this.position.x += (this.moveSpeedX / fps);
-    this.position.y += (this.moveSpeedY / fps);    
+    this.position.y += (this.moveSpeedY / fps);
   }
 
   this.zIndex = Math.floor(this.position.y);
-  
+
   this.planMove();
 
 }
 
-Hero.prototype.update = function  () {
+Hero.prototype.update = function() {
   var $this = this;
   $this.checkAction();
   switch ($this.status) {
-  case 'move':
-    $this.moveToPro(Engine.fps);
-  break
+    case 'move':
+      $this.moveToPro(Engine.fps);
+      break
 
-  case 'attack':
-    $this.attackPro(Engine.fps);
-  break
+    case 'attack':
+      $this.attackPro(Engine.fps);
+      break
 
-  case 'waiter':
-    this.planMove();
-  break  
+    case 'waiter':
+      this.planMove();
+      break
 
-  default:
+    default:
 
   }
 }
 
 
-Hero.prototype.attack = function  (enemy) {
+Hero.prototype.attack = function(enemy) {
   if (typeof enemy == "undefined") {
-    this.status = "waiter";      
+    this.status = "waiter";
     return
   }
 
@@ -434,10 +433,10 @@ Hero.prototype.attack = function  (enemy) {
   switch (type) {
     case 'physics':
       measure -= enemy.physicsDefense;
-    break
-    case 'magic':  
+      break
+    case 'magic':
       measure -= enemy.magicDefense;
-    break
+      break
     default:
   }
 
@@ -447,38 +446,38 @@ Hero.prototype.attack = function  (enemy) {
   this.attackInOut = false;
 }
 
-Hero.prototype.attackPro = function  (fps) {
+Hero.prototype.attackPro = function(fps) {
   var $this = this;
   var speed = $this.attackSpeed / fps;
-  
+
   //前摆
   this.planMove();
 
   var ca = Map.ctx;
 
-  ($this.attackProTime === undefined) ? $this.attackProTime = speed : $this.attackProTime += speed;
+  ($this.attackProTime === undefined) ? $this.attackProTime = speed: $this.attackProTime += speed;
 
   //画攻击
   ca.save();
-  ca.translate($this.position.x,$this.position.y);
+  ca.translate($this.position.x, $this.position.y);
   //ca.beginPath();
 
 
-  ca.fillStyle='rgba(255, 0, 0, '+ $this.attackProTime/$this.attackSpeed +')';
+  ca.fillStyle = 'rgba(255, 0, 0, ' + $this.attackProTime / $this.attackSpeed + ')';
 
   if ($this.direction == "left") {
     //ca.arc(-$this.size.width/1.15,-$this.size.height/1.15,10,0,Math.PI*2,true);
-    ca.fillRect(-$this.size.width/1.15 - 5, -$this.size.height/1.15, 30, $this.size.height/1.15);
+    ca.fillRect(-$this.size.width / 1.15 - 5, -$this.size.height / 1.15, 30, $this.size.height / 1.15);
   }
 
   if ($this.direction == "right") {
     //ca.arc($this.size.width/1.15,-$this.size.height/1.15,10,0,Math.PI*2,true);
-    ca.fillRect($this.size.width/1.15 - 25, -$this.size.height/1.15, 30, $this.size.height/1.15);
+    ca.fillRect($this.size.width / 1.15 - 25, -$this.size.height / 1.15, 30, $this.size.height / 1.15);
   }
-  
+
   ca.restore();
 
-  if ($this.attackProTime > $this.attackInTime && ($this.attackInOut == false)) {  
+  if ($this.attackProTime > $this.attackInTime && ($this.attackInOut == false)) {
     $this.enemy.lift -= $this.enemyMeasure;
     $this.attackInOut = true;
     if ($this.enemy.lift <= 0) {
@@ -495,22 +494,22 @@ Hero.prototype.attackPro = function  (fps) {
   if ($this.attackProTime > $this.attackSpeed) {
     $this.attackProTime = undefined;
     $this.status = "waiter";
-    
+
   }
 
 
 }
 
 
-Hero.prototype.dead = function  () {
+Hero.prototype.dead = function() {
   console.log('dead');
   var $this = this;
-  for (var i = 0;  i<Map.viewObjs.length ; i++) {
+  for (var i = 0; i < Map.viewObjs.length; i++) {
     //console.log(Map.viewObjs[i].zIndex);
     var obj = Map.viewObjs[i];
     if ($this == obj) {
       Map.viewObjs.splice(i);
-    }        
+    }
   }
   delete $this;
 
@@ -531,7 +530,7 @@ hero1.position.x = 150;
 hero1.position.y = 200;
 hero1.bgColor = "#f64";
 
-hero1.moveTo(200,200);
+hero1.moveTo(200, 200);
 
 hero2.position.x = Map.obj.width - 100;
 hero2.position.y = 200;
@@ -539,7 +538,7 @@ hero2.bgColor = "green";
 
 hero2.moveSpeed = 300;
 
-hero2.moveTo(Map.obj.width - 200,200);
+hero2.moveTo(Map.obj.width - 200, 200);
 
 
 
@@ -553,9 +552,9 @@ Engine.stop = false;
 Engine.fps = 60;
 
 
-Engine.getFps = function  () {
+Engine.getFps = function() {
   if ((this.now - this.lastTime) > 1000) {
-    this.fps = ((this.now - this.lastTime)/1000).toFixed(2) * 100;
+    this.fps = ((this.now - this.lastTime) / 1000).toFixed(2) * 100;
     if (this.fps < 30) {
       this.fps = 30;
     }
@@ -565,26 +564,26 @@ Engine.getFps = function  () {
 }
 
 
-Engine.requestAnimationFrame = function  (callback) {
+Engine.requestAnimationFrame = function(callback) {
   if (window.requestAnimationFrame) return window.requestAnimationFrame(callback);
   else if (window.webkitRequestAnimationFrame) return window.webkitRequestAnimationFrame(callback);
   else if (window.mozRequestAnimationFrame) return window.mozRequestAnimationFrame(callback);
   else {
-     return window.setTimeout(callback, 1000 / 60);
+    return window.setTimeout(callback, 1000 / 60);
   }
 }
 
-Engine.view = function  () {
-  
+Engine.view = function() {
+
 }
 
-Engine.animate = function  (time) {
+Engine.animate = function(time) {
   var $this = Engine;
 
   if ($this.stop == true) {
     return Engine.requestAnimationFrame(Engine.animate);
   }
-  
+
   $this.now = time || new Date().getTime();
   if ($this.lastTime == null) {
     $this.lastTime = $this.now;
